ed88d61b15cc23cd4fb56ab2c0468c78b2a3727d
Robin-Geissler <ga94koq@mytum.de>
2019-10-26 17:12:12 +0200

    write login data to "loginData.json" in data folder

--- moodleCrawl/saveLoginData.py
+++ moodleCrawl/saveLoginData.py
@@ -1,9 +1,27 @@
 import sys
+import cryptography
+import json
+
+
+def writedata(username, userpassword):
+    data = {'user': username, 'password': userpassword}
+
+    with open("../data/loginData.json", 'w') as storefile:
+        json.dump(data, storefile)
+
+
 
 user = sys.argv[1]
 passwort = sys.argv[2]
 
+writedata(user,passwort)
 print("the user is: " + user + "\nthe password is: " + passwort)
 print(True)
 
 sys.stdout.flush()
+
+
+
+
+
+
